e79c3901b5ae916df0ff3871c0eeff8407c530a7
bobvad <vladimirdegtannikov408@gmail.com>
2025-12-02 15:03:50 +0500

    Реализован класс BlackList Капустин

---
+++ Server/Classes/BlackList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Classes
+{
+    public class BlackList
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+    }
+}
